0695ad0750e3a5f318b2d969326232db3892d4fe
Subhadra Kumari <subhadraskills@gmail.com>
2025-09-11 09:52:09 +0530

    update

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 // src/lib/config.ts
 // export const prodURL = "https://joblinker-backend-c016.onrender.com" ;
-// export const LocalURl = "http://localhost:4000"
+// export const LocalURL = "http://localhost:4000"
 
 
-export const API_URL = "http://localhost:4000"
+export const API_URL = "https://joblinker-backend-c016.onrender.com"
